31125a9abeadbe9a46cbd8871a3dff0b71c17bc2
dain6978 <ekdls6978@naver.com>
2024-02-26 15:50:55 +0900

    Feat: GraphicSettingWidget (1)

--- Source/GLMP/Private/GraphicSetting.cpp
+++ Source/GLMP/Private/GraphicSetting.cpp
@@ -3,28 +3,82 @@
 
 #include "GraphicSetting.h"
 
+#include "Components/ComboBoxString.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "GameFramework/GameUserSettings.h"
 
 
 void UGraphicSetting::NativePreConstruct()
 {
+	if (ComboBox_Resolution)
+	{
+		ComboBox_Resolution->OnSelectionChanged.AddDynamic(this, &UGraphicSetting::ResolutionBoxOnChagned);
+	}
+	if (ComboBox_Quality)
+	{
+		ComboBox_Quality->OnSelectionChanged.AddDynamic(this, &UGraphicSetting::ResolutionBoxOnChagned);
+	}
+	if (ComboBox_ScreenMode)
+	{
+		ComboBox_ScreenMode->OnSelectionChanged.AddDynamic(this, &UGraphicSetting::ResolutionBoxOnChagned);
+	}
+
 	SetResolutionList();
 }
 
 void UGraphicSetting::SetResolutionList()
 {
 	UKismetSystemLibrary::GetSupportedFullscreenResolutions(Resolutions);
-	UGameUserSettings* setting = UGameUserSettings::GetGameUserSettings();
 
-	for (FIntPoint Resolution : Resolutions) 
+	for (FIntPoint Resolution : Resolutions)
 	{
 		FString ResolutionString;
 
 		ResolutionString.Append(FString::FromInt(Resolution.X));
-		ResolutionString.Append(FString::FromInt(" x "));
+		ResolutionString.Append(" x ");
 		ResolutionString.Append(FString::FromInt(Resolution.Y));
 
-		/*ComboBox_Resolution->AddOption(TEXT(ResolutionString));*/
+		ComboBox_Resolution->AddOption(ResolutionString);
+	}
+}
+
+void UGraphicSetting::ResolutionBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	int32 SelectedIndex = ComboBox_Resolution->GetSelectedIndex();
+
+	if (SelectedIndex >= 0)
+	{
+		UGameUserSettings* Setting = UGameUserSettings::GetGameUserSettings();
+		Setting->SetScreenResolution(Resolutions[SelectedIndex]);
+	}
+}
+
+void UGraphicSetting::QualityBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	int32 SelectedIndex = ComboBox_Quality->GetSelectedIndex();
+
+	if (SelectedIndex >= 0)
+	{
+		UGameUserSettings* Setting = UGameUserSettings::GetGameUserSettings();
+		Setting->SetOverallScalabilityLevel(SelectedIndex);
+	}
+}
+
+void UGraphicSetting::ScreenModeBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	int32 SelectedIndex = ComboBox_Quality->GetSelectedIndex();
+	UGameUserSettings* Setting = UGameUserSettings::GetGameUserSettings();
+
+	switch (SelectedIndex)
+	{
+	case 0:
+		Setting->SetFullscreenMode(EWindowMode::Fullscreen);
+		break;
+	case 1:
+		Setting->SetFullscreenMode(EWindowMode::Windowed);
+		break;
+	case 2:
+		Setting->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+		break;
 	}
 }
--- Source/GLMP/Public/GraphicSetting.h
+++ Source/GLMP/Public/GraphicSetting.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+
 #include "GraphicSetting.generated.h"
 
 /**
@@ -16,31 +17,42 @@ class GLMP_API UGraphicSetting : public UUserWidget
 
 protected:
 	// Variables
-	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	//class UButton* Button_Apply;
-	//
-	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	//class UButton* Button_Cancel;
-
-	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	//class UButton* Button_QuailtyDetail;
-
-	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	//class UComboBox* ComboBox_Quality;
-
-	UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	class UComboBox* ComboBox_Resolution;
-
-	//UPROPERTY(BlueprintReadWrite, meta = (BindWidget));
-	//class UComboBox* ComboBox_ScreenMode;
-	
-	UPROPERTY(BlueprintReadOnly, Category = "Graphic Setting");
 	TArray<FIntPoint> Resolutions;
 
-	
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UComboBoxString* ComboBox_Resolution;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UComboBoxString* ComboBox_Quality;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UComboBoxString* ComboBox_ScreenMode;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* Button_Apply;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* Button_Cancel;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* Button_ChangeSky;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* Button_QualityDetail;
+
+
 	// Functions
 	virtual void NativePreConstruct() override;
 
-
+	UFUNCTION()
 	void SetResolutionList();
+
+	UFUNCTION()
+	void ResolutionBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void QualityBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void ScreenModeBoxOnChagned(FString SelectedItem, ESelectInfo::Type SelectionType);
 };
