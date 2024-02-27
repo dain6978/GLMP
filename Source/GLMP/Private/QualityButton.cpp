fbefe163e9a433e9c2049af588a7f8abf5fd0c6c
dain6978 <ekdls6978@naver.com>
2024-02-27 22:07:47 +0900

    Feat: Quality Detail C++ Class (1)

--- Source/GLMP/Private/QualityButton.cpp
+++ Source/GLMP/Private/QualityButton.cpp
@@ -1,5 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "QualityButton.h"
 
+#include "Components/Button.h"
+#include "GameFramework/GameUserSettings.h"
+
+void UQualityButton::NativePreConstruct()
+{
+	if (Button)
+	{
+		Button->OnClicked.AddDynamic(this, &UQualityButton::OnClickedButton);
+	}
+}
+
+void UQualityButton::OnClickedButton()
+{
+	OnPressed();
+	UnPressed();
+	UGameUserSettings* Setting = UGameUserSettings::GetGameUserSettings();
+	Setting->ApplySettings(true);
+}
+
+void UQualityButton::OnPressed()
+{
+}
+
+void UQualityButton::UnPressed()
+{
+}
